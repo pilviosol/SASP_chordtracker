e96c0a4ae7ce89f2fa47513856baaa34fc0e68c1
andreaeugeniolosi <andrealosi93@yahoo.com>
2021-12-17 18:10:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ import pandas as pd
 import IPython
 import IPython.display as ipd
 import librosa
-import \
-    re  # regular expression, is a special sequence of characters that helps you match or find other strings or sets of strings
+
 import IPython  # listen to sounds on Python
 # import pretty_midi # contains utility function/classes for handling MIDI data, !install only if u want to listen the predictions!
 
@@ -37,49 +36,8 @@ plt.style.use('seaborn')
 
 
 # Variables
-path_lab = "E:/Uni/First year/Second Semester/Sound Analysis/Project Chord detection/Beatles_lab/"
+path_lab = 'data/Beatles_lab'
 notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-
-
-# simplify the chord notation
-def __simplify_chords(chords_df):  # silence
-    chords_processed = chords_df['chord'].str.split(':maj')  # remove major x chords return array of array
-    # containing all the chords
-    chords_processed = [elem[0] for elem in chords_processed]  # further process step above to return 1 array
-    # take the first element in all the N arrays (chords
-    # string) to make it a list of N elements
-    chords_processed = [elem.split('/')[0] for elem in chords_processed]  # remove inverted chords
-    chords_processed = [elem.split('aug')[0] for elem in chords_processed]  # remove augmented chords
-    chords_processed = [elem.split(':(')[0] for elem in chords_processed]  # remove added notes chords
-    chords_processed = [elem.split('(')[0] for elem in chords_processed]  # remove added notes chords 2
-    chords_processed = [elem.split(':sus')[0] for elem in chords_processed]  # remove sustained chords
-    chords_processed = [re.split(":?\d", elem)[0] for elem in chords_processed]  # remove added note
-    chords_processed = [elem.replace('dim', 'min') for elem in chords_processed]  # change diminute to minor
-    chords_processed = [elem.replace('hmin', 'min') for elem in chords_processed]  # change semi-diminute to minor
-    chords_processed = [re.split(":$", elem)[0] for elem in chords_processed]  # remove added notes chords
-    return chords_processed
-
-# Read lab files
-def readlab(path):
-    dictionary = []
-    list_name = []
-
-    for elem in os.listdir(path):
-        song_path = f'{path}/{elem}'
-        list_name.append(elem)
-        chord_annotation = pd.read_csv(song_path, sep=' ', header=None)  # no header in the files and separated
-        # by a empty space
-        # set up the columns of the file
-        chord_annotation.columns = ['start', 'end', 'chord']
-        chord_annotation['chord'] = __simplify_chords(chord_annotation)
-        chord_annotation.loc[chord_annotation['chord'] == 'N', 'chord'] = chord_annotation['chord'].mode()[
-            0]  # replace silence by probable
-        # tonal end
-        dictionary.append(chord_annotation)
-
-    return dictionary, list_name
-
-
 
 
 # Probabilità di avere un accordo dopo l'altro
@@ -103,12 +61,11 @@ def transition_prob_matrix(firstchord, secondchord):
     return prob_matrix
 
 
-chord_annotation_dic, song_list = readlab(path_lab)
+# create the dictionary from CSV
 
+
+# initialize the tp_matrix list
 tp_matrix = []
-
-
-
 
 
 for i,val in enumerate(chord_annotation_dic):
