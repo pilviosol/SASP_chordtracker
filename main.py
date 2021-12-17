0bd9b3af16cd8b874619b0beff8aa51457be4858
andreaeugeniolosi <andrealosi93@yahoo.com>
2021-12-17 20:49:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import pandas as pd
 import IPython
 import IPython.display as ipd
 import librosa
+import \
+    re
 
 import IPython  # listen to sounds on Python
 # import pretty_midi # contains utility function/classes for handling MIDI data, !install only if u want to listen the predictions!
@@ -37,7 +39,91 @@ plt.style.use('seaborn')
 
 # Variables
 path_lab = 'data/Beatles_lab'
-notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+path_csv = 'data/Beatles_csv/'
+
+notes = np.array(["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"])
+
+# chord_annotation_dic = pd.read_csv("data/chord_annotation_dic", sep=',', header=None)
+
+win_size_t = 2048/44100
+
+
+# simplify the chord notation
+def __simplify_chords(chords_df):  # silence
+    chords_processed = chords_df['chord'].str.split(':maj')  # remove major x chords return array of array
+    # containing all the chords
+    chords_processed = [elem[0] for elem in chords_processed]  # further process step above to return 1 array
+    # take the first element in all the N arrays (chords
+    # string) to make it a list of N elements
+    chords_processed = [elem.split('/')[0] for elem in chords_processed]  # remove inverted chords
+    chords_processed = [elem.split('aug')[0] for elem in chords_processed]  # remove augmented chords
+    chords_processed = [elem.split(':(')[0] for elem in chords_processed]  # remove added notes chords
+    chords_processed = [elem.split('(')[0] for elem in chords_processed]  # remove added notes chords 2
+    chords_processed = [elem.split(':sus')[0] for elem in chords_processed]  # remove sustained chords
+    chords_processed = [re.split(":?\d", elem)[0] for elem in chords_processed]  # remove added note
+    chords_processed = [elem.replace('dim', 'min') for elem in chords_processed]  # change diminute to minor
+    chords_processed = [elem.replace('hmin', 'min') for elem in chords_processed]  # change semi-diminute to minor
+    chords_processed = [re.split(":$", elem)[0] for elem in chords_processed]  # remove added notes chords
+    return chords_processed
+
+
+# Read lab files
+def readlab(path):
+    dictionary = []
+    list_name = []
+
+    for elem in os.listdir(path):
+        song_path = f'{path}/{elem}'
+        list_name.append(elem)
+        chord_annotation = pd.read_csv(song_path, sep=' ', header=None)  # no header in the files and separated
+                                                                         # by an empty space
+                                                                         # set up the columns of the file
+        chord_annotation.columns = ['start', 'end', 'chord']
+        chord_annotation['chord'] = __simplify_chords(chord_annotation)
+        chord_annotation.loc[chord_annotation['chord'] == 'N', 'chord'] = chord_annotation['chord'].mode()[
+            0]  # replace silence by probable
+                # tonal end
+        dictionary.append(chord_annotation)
+
+    return dictionary, list_name
+
+
+def readcsv_chroma(path):
+    dictionary = []
+
+    for elem in os.listdir(path):
+        song_path = f'{path}/{elem}'
+        chroma_annotation = pd.read_csv(song_path, sep=',', header=None)
+        chroma_annotation = pd.DataFrame.transpose(chroma_annotation)
+        chroma_annotation.columns = [notes]
+        dictionary.append(chroma_annotation)
+
+    return dictionary
+
+
+chord_annotation_dic, song_list = readlab(path_lab)
+chroma_dic = readcsv_chroma(path_csv)
+
+
+# assing chord to every chroma rows
+def chord_chroma_raws(chroma, chord_annotation):
+    chroma['chord'] = '0'
+    for idx, value in enumerate(chroma):
+        print(idx)
+        raw = 0
+        if win_size_t*np.float(idx+1) < chord_annotation['end'][raw]:
+            chroma['chord'][idx] = chord_annotation['chord'][raw]
+        else:
+            raw += 1
+
+    return chroma
+
+
+chroma_dic_new = []
+
+for idx, value in enumerate(chord_annotation_dic):
+    print(idx)
+    chroma_dic_new[idx] = chord_chroma_raws(chroma_dic[idx], chord_annotation_dic[idx])
 
 
 # Probabilità di avere un accordo dopo l'altro
@@ -61,14 +147,11 @@ def transition_prob_matrix(firstchord, secondchord):
     return prob_matrix
 
 
-# create the dictionary from CSV
-
-
 # initialize the tp_matrix list
 tp_matrix = []
 
 
-for i,val in enumerate(chord_annotation_dic):
+for i, val in enumerate(chord_annotation_dic):
     first_chord = chord_annotation_dic[i]['chord'].values[:-1].tolist()
     second_chord = chord_annotation_dic[i]['chord'][1:].tolist()
     tp_matrix.append(transition_prob_matrix(first_chord, second_chord))
