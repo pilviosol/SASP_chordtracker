94e0176ae5e02c6ba5b3c69b558945e102fc9887
PilvioSol <polsilvio@gmail.com>
2022-02-18 10:12:32 +0100

    midi_2_audio commented

--- midi_2_audio.py
+++ midi_2_audio.py
@@ -1,11 +1,22 @@
 from midi2audio import FluidSynth
-from sf2_loader import *
-
-#FluidSynth().play_midi('data/test.mid')
-FluidSynth("/Users/PilvioSol/Downloads/70s_sci-fi/70's Sci-fi.sf2").midi_to_audio('data/test.mid','data/test.mp3')
-
 from pysndfx import AudioEffectsChain
 
+# ------------------------------------------------------------------------------------------
+# VARIABLES
+# ------------------------------------------------------------------------------------------
+path_soundfont = "/Users/PilvioSol/Downloads/70s_sci-fi/70's Sci-fi.sf2"
+
+
+# ------------------------------------------------------------------------------------------
+# MIDI 2 AUDIO CONVERSION
+# ------------------------------------------------------------------------------------------
+FluidSynth(path_soundfont).midi_to_audio('data/test.mid','data/test.mp3')
+
+
+# ------------------------------------------------------------------------------------------
+# EFFECT DEFINITION AND APPLICATION
+# ------------------------------------------------------------------------------------------
+# Effect definition
 fx = (
     AudioEffectsChain()
     .highshelf()
@@ -14,8 +25,10 @@ fx = (
     .delay()
     .lowshelf()
 )
+
+
 infile = 'data/test.mp3'
 outfile = 'data/test_effect.mp3'
 
-# Apply phaser and reverb directly to an audio file.
+# Effect application
 fx(infile, outfile)
